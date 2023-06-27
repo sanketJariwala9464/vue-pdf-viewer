61d2a3b20e8348af6de67230a580fc078e898835
Sanket Jariwala <sanketjariwala45@gmail.com>
2023-06-27 17:13:27 +0530

    Update - add pen tool

--- src/components/PdfViewer.vue
+++ src/components/PdfViewer.vue
@@ -19,11 +19,37 @@
       </el-aside>
 
       <el-container>
-        <el-header class="bg-color-white">Header</el-header>
+        <el-header class="bg-color-white">
+          <el-popover
+            placement="bottom"
+            width="500"
+            trigger="hover"
+          >
+            <div>
+              <div class="block">
+                <span class="demonstration">Storke Width</span>
+                <el-slider v-model="settings.pen.width" :max="50"></el-slider>
+              </div>
+              <div class="block">
+                <span class="demonstration">Storke Opacity</span>
+                <el-slider v-model="settings.pen.opacity"></el-slider>
+              </div>
+              <div class="block">
+                <div class="demonstration">Color</div>
+                <el-color-picker v-model="settings.pen.color"></el-color-picker>
+              </div>
+            </div>
+            <el-button slot="reference" :type="!mode.ispen ? 'danger' : 'success'" @click="setPenMode()" class="download-button" size="small"><i class="el-icon-edit"></i></el-button>
+          </el-popover>
+          <!-- <button @click="setAddTextMode()" class="el-button download-button el-button--danger el-button--small is-plain el-dropdown-selfdefine">
+            <img src="../../public/text.png" alt="">
+          </button> -->
+        </el-header>
         
         <el-main>
+          <!-- {{ mode }} -->
           <div class="main-view">
-            <canvas ref="activepahe"></canvas>
+            <div id="PDF-stage" class="d-view-pdf"></div>
           </div>
         </el-main>
       </el-container>
@@ -34,6 +60,7 @@
 <script>
 import * as pdfjs from 'pdfjs-dist/legacy/build/pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.7.107/pdf.worker.js';
+import Konva from 'konva';
 
 export default {
   name: 'PdfViewer',
@@ -50,6 +77,21 @@ export default {
         totalPages: 0,
         currentPage: 1,
         scale: 1,
+        layer: {},
+        layerimage: {},
+        stage: {},
+        text: {},
+      },
+      mode: {
+        ispen: false,
+        isaddText: false,
+      },
+      settings: {
+        pen: {
+          color: '#df4b26',
+          width: 5,
+          opacity: 100,
+        },
       }
     };
   },
@@ -75,9 +117,34 @@ export default {
       }
     },
     async renderPage() {
+      const self = this;
       const page = await this.state.pdf.getPage(this.state.currentPage);
-      const canvas = this.$refs.activepahe;
-      page.render(await this.getPdfPage(page, canvas));
+      const canvas = document.createElement('canvas');
+      const renderContext = await this.getPdfPage(page, canvas);
+      const image = new Image();
+      image.src = await page.render(renderContext).promise.then(() => {
+        return canvas.toDataURL();
+      });
+      image.onload = function () {
+        self.stage = new Konva.Stage({
+          container: 'PDF-stage',
+          width: renderContext.viewport.width,
+          height: renderContext.viewport.height,
+          id: 'pdf-stage',
+        });
+
+        self.layer = new Konva.Layer({
+          draggable: false,
+        });
+        self.stage.add(self.layer);
+        self.layerimage = new Konva.Image();
+        self.text = new Konva.Text();
+        self.layer.add(self.layerimage);
+        self.layerimage.setImage(image);  
+        self.layer.batchDraw();
+        self.initPen();
+        self.initText();
+      };
     },
     async getPdfPage(page, canvas, issidebar = false) {
       const context = canvas.getContext('2d');
@@ -104,8 +171,100 @@ export default {
       return renderContext;
     },
     async openPage(pageNum) {
+      this.mode.ispen = false;
+      this.mode.isaddText = false;
       this.state.currentPage = pageNum;
       this.renderPage();
+      this.setCurson();
+    },
+    async setPenMode() {
+      this.mode.ispen = !this.mode.ispen;
+      if (this.mode.ispen) {
+        this.setCurson('pen');
+      } else {
+        this.setCurson();
+      }
+    },
+    async setAddTextMode() {
+      this.mode.isaddText = !this.mode.isaddText;
+      if (this.mode.isaddText) {
+        this.setCurson('text');
+      } else {
+        this.setCurson();
+      }
+    },
+    setCurson(type = '') {
+      let cursor = '';
+      switch (type) {
+        case 'pen':
+          cursor = 'url(../../pencile.cur), auto';
+        break;
+        case 'text':
+          cursor = 'text';
+        break;
+      }
+      const element = document.getElementById('PDF-stage');
+      if (cursor !== '' && type !== '') {
+        element.style.cursor = cursor;
+      } else {
+        element.style.cursor = 'default';
+      }
+    },
+    initPen() {
+      let isDrawing = false;
+      const self = this;
+      self.stage.on('mousedown touchstart', function () {
+        console.log('mousedown ')
+        if (self.mode.ispen) {
+          isDrawing = true;
+          const pos = self.stage.getPointerPosition();
+          self.lastLine = new Konva.Line({
+            stroke: self.settings.pen.color,
+            strokeWidth: self.settings.pen.width,
+            opacity: self.settings.pen.opacity / 100,
+            globalCompositeOperation: 'source-over',
+            lineCap: 'round',
+            lineJoin: 'round',
+            points: [pos.x, pos.y, pos.x, pos.y],
+            draggable: true,
+          });
+          self.layer.add(self.lastLine);
+        }
+      });
+
+      self.stage.on('mouseup touchend', function () {
+        isDrawing = false;
+      });
+
+      self.stage.on('mousemove touchmove', function (e) {
+        if (!self.mode.ispen) return;
+        if (!isDrawing) return;
+        e.evt.preventDefault();
+        const pos = self.stage.getPointerPosition();
+        const newPoints = self.lastLine.points().concat([pos.x, pos.y]);
+        self.lastLine.points(newPoints);
+      });
+    },
+    initText() {
+      const self = this;
+      self.stage.on("click tap", function () {
+        if (self.mode.isaddText) {
+          const pos = self.stage.getPointerPosition();
+          self.text.setAttrs({
+            x: pos.x,
+            y: pos.y,
+            width: 100,
+            height: 20,
+            fontSize: 20,
+            text: 'Some text here',
+            draggable: true,
+            id: 'text',
+          });
+          self.layer.add(self.text);
+
+          self.setAddTextMode();
+        }
+      });
     }
   },
 };
@@ -153,6 +312,9 @@ export default {
 
 .bg-color-white {
   background-color: #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .pdf-number {
